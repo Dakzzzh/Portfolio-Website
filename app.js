7232d9cf2033e985667205a1ef4a56e4e6049046
Dakzzzh <154968283+Dakzzzh@users.noreply.github.com>
2023-12-29 16:13:02 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function openNav(){
+    document.getElementById("mySideNav").style.width = "250px";
+}
+
+function closeNav(){
+    document.getElementById("mySideNav").style.width = "0px";
+}
+
+function openKPUwebsite(){
+    window.open("https://www.kpu.ca/", "_blank");
+}
+
+function openPythonWebsite(){
+    window.open("https://www.python.org", "_blank");
+}
+
+function openHTMLlink(){
+    window.open("https://html.com/html5/", "_blank");
+}
+
+function openCSSlink(){
+    window.open("https://en.wikipedia.org/wiki/CSS", "_blank");
+}
+
+function openJSlink(){
+    window.open("https://www.javascript.com", "_blank");
+}
